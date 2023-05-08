e01d02830228a47ea21db08ce0fff619febca935
7lad48 <2335213@gmail.com>
2023-05-08 17:25:32 +0300

    native js introducing to test

--- src/01-hello-test/01.test.ts
+++ src/01-hello-test/01.test.ts
@@ -1,10 +1,93 @@
+import {mult, splitIntoWords, sum} from "./01";
+
+let a: number;
+let b: number;
+let c: number;
+
+
+beforeEach( () => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
 test('sum should be corrected', () => {
     //data
-    const a = 1
-    const b = 2
-    const c = 3
 
     //action
     const result = sum(a,b)
+    a = 100
+    const result2 = sum(a,b)
+
+    //expect result
+    expect(result).toBe(3)
+    expect(result2).toBe(102)
+
+})
+
+
+test('multiply should be corrected', () => {
+    //data
+
+    //action
+    const result = mult(a,b)
+    const result2 = mult(b,c)
+
+    //expect result
+    expect(result).toBe(2)
+    expect(result2).toBe(6)
+
+})
+
+test('splitting into words should be correct', () => {
+    //data
+    const sent1 = 'Hello my friend'
+    const sent2 = 'JS - the best programming language.'
+    //action
+    const result1 = splitIntoWords(sent1);
+    const result2 = splitIntoWords(sent2);
+    //expected result
+    expect(result1.length).toBe(3)
+    expect(result1[0]).toBe('hello')
+    expect(result1[1]).toBe('my')
+    expect(result1[2]).toBe('friend')
+
+    expect(result2.length).toBe(5)
+    expect(result2[0]).toBe('js')
+    expect(result2[1]).toBe('the')
+    expect(result2[2]).toBe('best')
+    expect(result2[3]).toBe('programming')
+    expect(result2[4]).toBe('language')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
